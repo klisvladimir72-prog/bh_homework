d9efc62f643b4ef0c29761bd9374630c9123bde2
Владимир Клиш <klish91@inbox.ru>
2025-12-10 19:27:00 +0300

    add lesson_12

--- lesson_12/1.py
+++ lesson_12/1.py
@@ -13,3 +13,57 @@ says таким образом, чтобы он возвращал строку 
 - "{кличка} - корова. Говорит МУ!" для класса Cow
 """
 
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    Абстрактный метод представляющий животное
+
+    :param name: Кличка животного
+    :type name: str
+    """
+
+    def __init__(self, name: str):
+        self.name = name
+
+    @abstractmethod
+    def says():
+        """
+        Абстрактный метод для звуков животного
+        """
+        pass
+
+
+class Cat(Animal):
+    """
+    :return голос кота
+    """
+
+    def says(self):
+        return f"{self.name} - говорит МЯУ!"
+
+
+class Dog(Animal):
+    """
+    :return голос собаки
+    """
+
+    def says(self):
+        return f"{self.name} - говорит ГАВ!"
+
+
+class Cow(Animal):
+    """
+    :return голос коровы
+    """
+
+    def says(self):
+        return f"{self.name} - говорит МУУ!"
+
+
+animals = [Cat("Мурка"), Dog("Шарик"), Cow("Бурёнка")]
+
+
+for animal in animals:
+    print(animal.says())
--- lesson_12/2.py
+++ lesson_12/2.py
@@ -12,3 +12,105 @@
 
 Аксессоры реализоваться через property.
 """
+
+import datetime
+from typing import Optional
+from pprint import pprint
+
+
+class BookCard:
+    """
+    Класс для представления карточки книги
+
+      :param author: Имя автора
+      :type author: str
+      :param title: Название книги
+      :type title: str
+      :param year: Год издания книги
+      :type year: int
+    """
+
+    def __init__(self, author: str, title: str, year: int):
+        self.author = author
+        self.title = title
+        self.year = year
+
+    @property
+    def author(self) -> str:
+        return self._author.strip().title()
+
+    @author.setter
+    def author(self, value: str):
+        if not isinstance(value, str):
+            raise ValueError(f"Должна быть строка, передано - {type(value)}")
+        self._author = value.strip().title()
+
+    @property
+    def title(self) -> str:
+        return self._title.strip().capitalize()
+
+    @title.setter
+    def title(self, value: str):
+        if not isinstance(value, str):
+            raise ValueError(f"Должна быть строка, передано - {type(value)}")
+        self._title = value.strip().capitalize()
+
+    @property
+    def year(self) -> int:
+        return self._year
+
+    @year.setter
+    def year(self, value: int):
+        if not isinstance(value, int):
+            raise ValueError(f"Должно быть целое число, передано - {type(value)}")
+
+        current_year = datetime.datetime.now().year
+        if current_year < value <= 0:
+            raise ValueError(f"Год {value} должен быть реальным.")
+
+        self._year = value
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, BookCard):
+            raise NotImplemented
+        return self.year == other.year
+
+    def __ne__(self, other) -> bool:
+        if not isinstance(other, BookCard):
+            raise NotImplemented
+        return self.year != other.year
+
+    def __lt__(self, other) -> bool:
+        if not isinstance(other, BookCard):
+            raise NotImplemented
+        return self.year < other.year
+
+    def __le__(self, other) -> bool:
+        if not isinstance(other, BookCard):
+            raise NotImplemented
+        return self.year <= other.year
+
+    def __gt__(self, other) -> bool:
+        if not isinstance(other, BookCard):
+            raise NotImplemented
+        return self.year > other.year
+
+    def __ge__(self, other) -> bool:
+        if not isinstance(other, BookCard):
+            raise NotImplemented
+        return self.year >= other.year
+
+    def __repr__(self) -> str:
+        return f"BookCard(author='{self.author}', title='{self.title}', year='{self.year}')"
+
+
+books = [
+    BookCard("Толстой", "Война и мир", 1869),
+    BookCard("Достоевский", "Преступление и наказание", 1866),
+    BookCard("Оруэлл", "1984", 1949),
+    BookCard("Харпер Ли", "Убить пересмешника", 1960),
+]
+
+pprint(sorted(books))
+print()
+pprint(sorted(books, reverse=True))
